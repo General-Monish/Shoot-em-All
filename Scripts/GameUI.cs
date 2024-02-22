97ca5660281f5f05a1a76adae15ecd905c9b68c5
General-Monish <monissam28@gmail.com>
2024-02-23 00:46:37 +0530

    Added WaveBanner To tell Level

--- Scripts/GameUI.cs
+++ Scripts/GameUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,10 +9,57 @@ public class GameUI : MonoBehaviour
 {
     public Image fadePLane;
     public GameObject gameOverUI;
+
+    public RectTransform newWaveBanner;
+    public TextMeshProUGUI waveCount;
+    public TextMeshProUGUI enemiesCount;
+
+    Spawner spawner;
+
+    private void Awake()
+    {
+        spawner = FindAnyObjectByType<Spawner>();
+        spawner.OnNewWave += OnNewWave;
+    }
+
+    public void OnNewWave(int waveNumber)
+    {
+        string[] numbers = { "One", "Two", "Three", "Four", "Five" };
+        waveCount.text = "Wave:" + numbers[waveNumber - 1];
+        string enemyCountString = ((spawner.waves[waveNumber - 1].infinite) ? "Infinite" : spawner.waves[waveNumber - 1].enemyCount+"");
+        enemiesCount.text = "Enemies:" + enemyCountString;
+
+        StartCoroutine("AnimateNewWaveBanner");
+        StartCoroutine("AnimateNewWaveBanner");
+    }
     // Start is called before the first frame update
     void Start()
     {
         FindAnyObjectByType<Player>().onDeath += GameUI_onDeath;
+    }
+
+    IEnumerator AnimateNewWaveBanner()
+    {
+        float delayTime=4f;
+        float animatePercent = 0;
+        float speed=1f;
+        int dir = 1;
+
+        float endDelayTime = Time.time + 1 / speed + delayTime;
+        while (animatePercent>=0)
+        {
+            animatePercent += Time.deltaTime * speed * dir;
+            if (animatePercent >= 1)
+            {
+                animatePercent = 1;
+                if (Time.time > endDelayTime)
+                {
+                    dir = -1;
+                }
+            }
+            newWaveBanner.anchoredPosition = Vector2.up * Mathf.Lerp(-255, 840, animatePercent);
+            yield return null;
+        }
     }
 
     private void GameUI_onDeath()
